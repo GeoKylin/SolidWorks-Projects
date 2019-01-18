b361efc32da844b0f6be41b04dff23f6bfe61f71
GeoFox-bingyan <wangkai_geo@163.com>
2019-01-18 20:35:23 +0800

    2019/1/18

---
+++ mygit.sh
@@ -0,0 +1,11 @@
+# git init
+
+git add *.SLDPRT Study* *.sh
+
+git status
+
+git commit -m "2019/1/18"
+git remote add origin git@github.com:GeoKylin/SolidWorks-Projects.git
+
+git pull --rebase origin master
+git push -u origin master
